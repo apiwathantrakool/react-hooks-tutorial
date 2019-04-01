64ea79546b3f943ac4b1541e1354867b9797010b
apiwat.h <apiwat.h@aware.co.th>
2019-04-01 10:32:49 +0700

    [Section03:42]: useState() Hook
    use state and set state in const component.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './components/person'
 
-class App extends Component {
+const app = props => {
 
-  state = {
+  const [ personsState, setPersonsState  ] = useState({
     persons: [
       { name: 'Zaza', age: 6 },
       { name: 'Zee', age: 21 },
       { name: 'Zoe', age: 20 }
-    ],
-    otherState: 'some other value'
-  }
+    ]
+  })
 
-  switchNameHandler = () => {
-    this.setState({
+  const [ otherState, setOtherState ] = useState({
+    otherState: 'some other value'
+  })
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'Zaza', age: 97 },
         { name: 'Zee', age: 98 },
@@ -23,30 +26,22 @@ class App extends Component {
     })
   }
 
-  render() {
-    return (
-      <div>
-        <Person name='Nekky' age='22'></Person>
-        <Person name='NJ' age='21'>
-          So cute
+  console.log(personsState, otherState);
+
+  return (
+    <div>
+      <Person name='Nekky' age='22'></Person>
+      <Person name='NJ' age='21'>
+        So cute
         </Person>
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
 
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-      </div>
-      /* return React.createElement('div', null, 
-                  React.createElement('h1',null, Hi) , 'Hello Worl'); */
-
-      // JSX compile, React will complie the HTML code to JS code by using
-      // the comment code above.
-
-      // JSX return, React will require wrapping html, because when it complie to JS.
-      // It will compile DOM to be nested JS like code above.
-    );
-  }
+      <button onClick={switchNameHandler}>Switch Name</button>
+    </div>
+  );
 }
 
-export default App;
+export default app;
