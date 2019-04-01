f23ff3e7a74e00b10ea6541ba81e1cc0d3fd2c83
apiwat.h <apiwat.h@aware.co.th>
2019-04-01 09:55:07 +0700

    Multiple state.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,10 +6,21 @@ class App extends Component {
 
   state = {
     persons: [
-      {name: 'Zaza', age: 6},
-      {name: 'Zee', age: 21},
-      {name: 'Zoe', age: 20}
-    ]
+      { name: 'Zaza', age: 6 },
+      { name: 'Zee', age: 21 },
+      { name: 'Zoe', age: 20 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Zaza', age: 97 },
+        { name: 'Zee', age: 98 },
+        { name: 'Zoe', age: 99 }
+      ]
+    })
   }
 
   render() {
@@ -20,19 +31,21 @@ class App extends Component {
           So cute
         </Person>
 
-        <Person name= {this.state.persons[0].name} age= {this.state.persons[0].age}/>
-        <Person name= {this.state.persons[1].name} age= {this.state.persons[1].age}/>
-        <Person name= {this.state.persons[2].name} age= {this.state.persons[2].age}/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
+        <button onClick={this.switchNameHandler}>Switch Name</button>
       </div>
-    /* return React.createElement('div', null, 
-                React.createElement('h1',null, Hi) , 'Hello Worl'); */
-  
-    // JSX compile, React will complie the HTML code to JS code by using
-    // the comment code above.
-  
-    // JSX return, React will require wrapping html, because when it complie to JS.
-    // It will compile DOM to be nested JS like code above.
-   );
+      /* return React.createElement('div', null, 
+                  React.createElement('h1',null, Hi) , 'Hello Worl'); */
+
+      // JSX compile, React will complie the HTML code to JS code by using
+      // the comment code above.
+
+      // JSX return, React will require wrapping html, because when it complie to JS.
+      // It will compile DOM to be nested JS like code above.
+    );
   }
 }
 
