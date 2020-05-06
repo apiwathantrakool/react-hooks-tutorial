6b2814c220e300438a89482b97a9b8b20c4ddcd6
Apiwat.h (Nekky) <apiwat_hantrakool@cmu.ac.th>
2020-05-06 23:57:07 +0700

    Add CSS dynamic styling

--- my-app/src/components/person.js
+++ my-app/src/components/person.js
@@ -1,16 +1,30 @@
 import React from "react";
+import "./person.css";
 
 const person = (props) => {
   const { name, age, onChangeName } = props;
+  const txtStyleClasses = getTxtStyleClasses(name);
   return (
     <div>
-      <p>
+      <p className={txtStyleClasses}>
         My name is {name} and I am {age}
       </p>
       <p>{props.children}</p>
       <input onChange={onChangeName} value={name} />
     </div>
   );
+};
+
+const getTxtStyleClasses = (name) => {
+  const txtStyle = [];
+  if (name.length >= 5) {
+    txtStyle.push("text-bold");
+  }
+  if (name.length >= 10) {
+    txtStyle.push("background-red");
+  }
+  // className="text-bold background-red"
+  return txtStyle.join(" ");
 };
 
 export default person;
